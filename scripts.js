d242d3e5c28b44306e53954ed86a2a36452e8be2
Yeray <ygl0004@.medac.es>
2024-12-18 18:49:11 +0100

    -Añadidos comentarios al HTML. -Implementado un selector de imagenes en el carrusel. -Ajustes menores al estilo de la cabecera y la portada principal.

--- scripts.js
+++ scripts.js
@@ -2,19 +2,39 @@ document.addEventListener("DOMContentLoaded", function () {
   // ******** SELECCIÓN DE ELEMENTOS DEL CARRUSEL ********
   const images = document.querySelectorAll(".carrusel_imagen");
   const productInfos = document.querySelectorAll(".info_producto");
+  const indicadores = document.querySelectorAll(".indicador");
   let currentIndex = 0;
 
   // ******** FUNCIÓN PARA MOSTRAR LA SIGUIENTE IMAGEN ********
   function showNextImage() {
     images[currentIndex].classList.remove("active");
     productInfos[currentIndex].classList.remove("active");
+    indicadores[currentIndex].classList.remove("active");
+
     currentIndex = (currentIndex + 1) % images.length;
+
     images[currentIndex].classList.add("active");
     productInfos[currentIndex].classList.add("active");
+    indicadores[currentIndex].classList.add("active");
   }
 
   // ******** INTERVALO PARA CAMBIAR LA IMAGEN CADA 8 SEGUNDOS ********
   setInterval(showNextImage, 8000);
+
+  // ******** FUNCIÓN PARA CAMBIAR DE IMAGEN AL CLICAR EN UN INDICADOR ********
+  indicadores.forEach((indicador, index) => {
+    indicador.addEventListener("click", function () {
+      images[currentIndex].classList.remove("active");
+      productInfos[currentIndex].classList.remove("active");
+      indicadores[currentIndex].classList.remove("active");
+
+      currentIndex = index;
+
+      images[currentIndex].classList.add("active");
+      productInfos[currentIndex].classList.add("active");
+      indicadores[currentIndex].classList.add("active");
+    });
+  });
 
   // ******** SELECCIÓN DEL ENCABEZADO ********
   const header = document.getElementById("encabezado");
